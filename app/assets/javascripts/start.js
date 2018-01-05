9f7f1e2dd1996c20d047e3a610caa6e485e761e7
Dav <dav2070@web.de>
2018-01-05 18:01:14 +0100

    Add styles for start and add navbar

---
+++ app/assets/javascripts/start.js
@@ -0,0 +1,14 @@
+$(function() {
+   arrangeElements();
+	$(window).resize(function() {
+      arrangeElements();
+   });
+});
+
+function arrangeElements(){
+   // Set the height of the first image
+   var height = $(window).height();
+   var width = $(window).width();
+
+   $("#start-section-1").height(height);
+}
