1135aee5c0502636c9a5990518d0afb938fa7aec
Dav <dav2070@web.de>
2018-01-06 16:16:59 +0100

    Fix a bug where jquery didn't want to load!

---
+++ public/assets/scripts/navbar-73461444127be555901d88c98b095bc9ae1c99edc871b765979fa8856ca3adcb.js
@@ -0,0 +1,14 @@
+$(function($){
+   // Collapse now if page is not at top
+   navbarCollapse();
+   // Collapse the navbar when page is scrolled
+   $(window).scroll(navbarCollapse);
+});
+
+function navbarCollapse(){
+   if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").css("background-color", "rgba(255, 255, 255, 0.87)");
+   } else {
+      $("#mainNav").css("background-color", "transparent");
+   }
+};
