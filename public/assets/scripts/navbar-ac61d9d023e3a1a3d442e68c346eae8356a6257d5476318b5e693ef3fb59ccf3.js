65bacc81d8f77ccfb618c2b88501459135b5c407
David <dav2070@web.de>
2020-05-28 10:28:24 +0200

    Precompile assets for production

---
+++ public/assets/scripts/navbar-ac61d9d023e3a1a3d442e68c346eae8356a6257d5476318b5e693ef3fb59ccf3.js
@@ -0,0 +1,29 @@
+$(function(){
+   // Collapse now if page is not at top
+	navbarCollapse();
+	onSizeChange();
+   // Collapse the navbar when page is scrolled
+   $(window).scroll(navbarCollapse);
+	$(window).on('resize', onSizeChange);
+});
+
+function navbarCollapse(){
+   if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("acrylic light shadow");
+   } else {
+      $("#mainNav").removeClass("acrylic light shadow");
+   }
+};
+
+function onSizeChange(){
+	$('.navbar-collapse').collapse('hide');
+	
+	if(window.innerWidth > 575){
+		// Remove classes from navbar list to remove the acrylic
+		$("#navbar-list").removeClass("acrylic light shadow");
+	}else{
+		// Add classes to navbar list to make it acrylic
+		$("#navbar-list").addClass("acrylic light shadow");
+	}
+}
+;
